e44993469e4b380b3d7b4b0974ba8446d41f6307
Ryan Garro <rg171503@gmail.com>
2023-07-22 11:28:42 -0400

    forgot to save

--- BE/Quiz/Quiz/Controllers/QuestionController.cs
+++ BE/Quiz/Quiz/Controllers/QuestionController.cs
@@ -1,6 +1,26 @@
-﻿namespace Quiz.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Quiz.Contracts;
+using Quiz.Models;
+
+namespace Quiz.Controllers
 {
-    public class QuestionController
+    [ApiController]
+    [Route("api/question")]
+    public class QuestionController : ControllerBase
     {
+        private readonly IQuestionManager _questionManager;
+
+        public QuestionController(IQuestionManager questionManager)
+        {
+            _questionManager = questionManager;
+        }
+
+        [HttpGet("{category}")]
+        public async Task<ActionResult> GetQuestionsByCategoryName(string category)
+        {
+            List<QuestionModel> categories = await _questionManager.GetQuestionsByCategoryName(category);
+            return new JsonResult(categories);
+        }
     }
+
 }
