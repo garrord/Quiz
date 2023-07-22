da2bf6eab6dc4b0c383c4b16ed468c63ec2b64d2
Ryan Garro <rg171503@gmail.com>
2023-07-22 11:34:26 -0400

    added data, DI/IOC, dbseeder

---
+++ BE/Quiz/Quiz/Data/DbSeeder.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Quiz.Entities.DbContexts;
+using Quiz.Entities;
+
+namespace Quiz.Data
+{
+    public static class DbSeeder
+    {
+        public static void SeedCategoryData()
+        {
+            using (QuizContext context = new QuizContext(new DbContextOptionsBuilder<QuizContext>().
+                UseSqlServer("Server=localhost\\MSSQLSERVER04;Database=Quiz;Trusted_Connection=True;TrustServerCertificate=True").Options))
+            {
+                using (var connection = context.Database.GetDbConnection())
+                {
+                    connection.Open();
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SET IDENTITY_INSERT dbo.Categories ON;";
+                        command.ExecuteNonQuery();
+                    }
+
+                    string dataFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Categories.json");
+                    string json = File.ReadAllText(dataFilePath);
+                    List<Category> categories = JsonConvert.DeserializeObject<List<Category>>(json);
+                    //List<Category> categories = JsonConvert.DeserializeObject<List<Category>>(json);
+                    context.Categories.AddRange(categories);
+                    context.SaveChanges();
+
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SET IDENTITY_INSERT dbo.Categories OFF;";
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+        public static void SeedQuestionData()
+        {
+            using (QuizContext context = new QuizContext(new DbContextOptionsBuilder<QuizContext>().
+                UseSqlServer("Server=localhost\\MSSQLSERVER04;Database=Quiz;Trusted_Connection=True;TrustServerCertificate=True").Options))
+            {
+                using (var connection = context.Database.GetDbConnection())
+                {
+                    connection.Open();
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SET IDENTITY_INSERT dbo.Questions ON;";
+                        command.ExecuteNonQuery();
+                    }
+
+                    string dataFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Questions.json");
+                    string json = File.ReadAllText(dataFilePath);
+                    List<Question> questions = JsonConvert.DeserializeObject<List<Question>>(json);
+
+                    context.Questions.AddRange(questions);
+                    context.SaveChanges();
+
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SET IDENTITY_INSERT dbo.Questions OFF;";
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        public static void SeedAnswerData()
+        {
+            using (QuizContext context = new QuizContext(new DbContextOptionsBuilder<QuizContext>().
+                UseSqlServer("Server=localhost\\MSSQLSERVER04;Database=Quiz;Trusted_Connection=True;TrustServerCertificate=True").Options))
+            {
+                using (var connection = context.Database.GetDbConnection())
+                {
+                    connection.Open();
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SET IDENTITY_INSERT dbo.Answers ON;";
+                        command.ExecuteNonQuery();
+                    }
+
+                    string dataFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Answers.json");
+                    string json = File.ReadAllText(dataFilePath);
+                    List<Answer> answers = JsonConvert.DeserializeObject<List<Answer>>(json);
+
+                    context.Answers.AddRange(answers);
+                    context.SaveChanges();
+
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SET IDENTITY_INSERT dbo.Answers OFF;";
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    }
+}
--- BE/Quiz/Quiz/Program.cs
+++ BE/Quiz/Quiz/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Quiz.Contracts;
+using Quiz.Entities.DbContexts;
+using Quiz.Managers;
+using Quiz.QueryRepositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,15 +12,33 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<QuizContext>(dbContextOptions => dbContextOptions
+    .UseSqlServer("Server=localhost\\MSSQLSERVER04;Database=Quiz;Trusted_Connection=True;TrustServerCertificate=True"));
+builder.Services.AddTransient<ICategoryQueryRepository, CategoryQueryRepository>();
+builder.Services.AddTransient<ICategoryManager, CategoryManager>();
+builder.Services.AddTransient<IQuestionQueryRepository, QuestionQueryRepository>();
+builder.Services.AddTransient<IQuestionManager, QuestionManager>();
+builder.Services.AddCors(options => options.AddPolicy("AllowSpecificOrigin",
+    builder => builder.WithOrigins("*")
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors("AllowSpecificOrigin");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//DbSeeder.SeedCategoryData();
+//DbSeeder.SeedQuestionData();
+//DbSeeder.SeedAnswerData();
 
 app.UseHttpsRedirection();
 
